8b7b67bd1b0155c0b1558d15d4fa713b19324320
Pradeep Kumar Tippa <pktippa@gmail.com>
2021-01-02 12:29:29 +0530

    updated other comps to react-native-paper

--- mobile/src/components/event.component.js
+++ mobile/src/components/event.component.js
@@ -1,18 +1,6 @@
 import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  TouchableHighlight,
-  FlatList,
-  TouchableOpacity,
-} from 'react-native';
-import {TextInput, Button, Modal, Portal} from 'react-native-paper';
+import {TextInput, Button, Modal, Portal, List} from 'react-native-paper';
 import {Provider, connect, useDispatch} from 'react-redux';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {store} from '../store';
 import {addEvent} from '../store/event/action';
 
@@ -38,53 +26,28 @@ const EventComponent = (props) => {
   };
   return (
     <>
-      <StatusBar barStyle="dark-content" />
       <Provider store={store}>
-        <SafeAreaView>
-          <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={styles.scrollView}>
-            <View style={styles.body}>
-              <View style={styles.sectionContainer}>
-                <View style={styles.button}>
-                  <TouchableHighlight
-                    style={styles.openButton}
-                    onPress={onShowAddEventModal}>
-                    <Text style={styles.textStyle}>Add Event</Text>
-                  </TouchableHighlight>
-                  <AddEventComponent
-                    isVisible={addEventModal}
-                    onSubmit={onModalAddEventSubmit}
-                    onCancel={onModalCancel}
-                  />
-                </View>
-              </View>
-              <View style={styles.sectionContainer}>
-                <View style={styles.container}>
-                  <FlatList
-                    data={event['events']}
-                    renderItem={({item}) => (
-                      <TouchableOpacity
-                        onPress={() =>
-                          navigation.navigate('ItemList', {
-                            items: item.items,
-                            eventName: item.name,
-                          })
-                        }>
-                        <View>
-                          <Text style={styles.item}>Name: {item.name}</Text>
-                          <Text style={styles.item}>
-                            Description: {item.description}
-                          </Text>
-                        </View>
-                      </TouchableOpacity>
-                    )}
-                  />
-                </View>
-              </View>
-            </View>
-          </ScrollView>
-        </SafeAreaView>
+          <Button mode="contained" onPress={onShowAddEventModal}>
+            Add Event
+          </Button>
+          <AddEventComponent
+            isVisible={addEventModal}
+            onSubmit={onModalAddEventSubmit}
+            onCancel={onModalCancel}
+          />
+          {(event['events'] && (
+            <List.Section>
+              {event['events'].map((item) => (
+                <List.Item title={item.name} description={item.description} onPress={() =>
+                  navigation.navigate('ItemList', {
+                    items: item.items,
+                    eventName: item.name,
+                  })
+                }/>
+              ))}
+            </List.Section>
+          )) ||
+            null}
       </Provider>
     </>
   );
@@ -95,130 +58,29 @@ const AddEventComponent = (props) => {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   return (
-    <View style={styles.centeredView}>
-      <Portal>
-      <Modal
-        visible={isVisible}
-        onDismiss={onCancel}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text>Add Event</Text>
-            <TextInput
-              label="Event name"
-              onChangeText={(text) => setName(text)}></TextInput>
-            <TextInput
-              label="Event Description"
-              onChangeText={(text) => setDescription(text)}></TextInput>
+    <Portal>
+      <Modal visible={isVisible} onDismiss={onCancel}>
+        <TextInput
+          label="Event name"
+          onChangeText={(text) => setName(text)}></TextInput>
+        <TextInput
+          label="Event Description"
+          onChangeText={(text) => setDescription(text)}></TextInput>
 
-            <TouchableHighlight
-              style={{...styles.openButton, backgroundColor: '#2196F3'}}
-              onPress={onCancel}>
-              <Text style={styles.textStyle}>Cancel</Text>
-            </TouchableHighlight>
-            <Button  mode="contained" onPress={onCancel}>Cancel</Button>
-            <TouchableHighlight
-              style={styles.openButton}
-              onPress={() => {
-                onSubmit({name, description, items: []});
-              }}>
-              <Text style={styles.textStyle}>Submit</Text>
-            </TouchableHighlight>
-          </View>
-        </View>
+        <Button mode="contained" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button
+          mode="contained"
+          onPress={() => {
+            onSubmit({name, description, items: []});
+          }}>
+          Submit
+        </Button>
       </Modal>
-      </Portal>
-    </View>
+    </Portal>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  button: {
-    color: Colors.dark,
-    height: 100,
-    width: 200,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 20,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  openButton: {
-    backgroundColor: '#F194FF',
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
-  },
-});
 
 const mapStateToProps = (state) => ({
   event: state.event,
--- mobile/src/components/item-list.component.js
+++ mobile/src/components/item-list.component.js
@@ -1,73 +1,9 @@
 import React, {useState} from 'react';
-import {
-  FlatList,
-  Modal,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {View} from 'react-native';
+import {Modal, Button, Text, TextInput, List, Portal} from 'react-native-paper';
 import {connect, useDispatch} from 'react-redux';
 import {addItem, updateItem} from '../store/item/action';
 
-const styles = StyleSheet.create({
-  openButton: {
-    backgroundColor: '#F194FF',
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  container: {
-    flex: 1,
-    paddingTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 20,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  costStyle: {
-    height: 50,
-  },
-  addAndTotalFlex: {
-    height: 60,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-});
 const ItemListComponent = (props) => {
   const [addItemModal, setAddItemModal] = useState(false);
   const {route, event} = props;
@@ -80,19 +16,8 @@ const ItemListComponent = (props) => {
   };
   return (
     <View>
-      {/* <View style={{flex: 1, flexDirection: 'row', height: 100}}> */}
-      <View>
-        <View>
-          <TouchableOpacity
-            style={styles.openButton}
-            onPress={() => setAddItemModal(true)}>
-            <Text style={{alignItems: 'center'}}>Add</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.costStyle}>
-          <Text>Total Cost : {totalCost}</Text>
-        </View>
-      </View>
+      <Button mode="contained" onPress={() => setAddItemModal(true)}>Add</Button>
+      <Text>Total Cost : {totalCost}</Text>
       <AddItemComponent
         isVisible={addItemModal}
         onCancel={() => {
@@ -104,20 +29,20 @@ const ItemListComponent = (props) => {
           dispatch(addItem({name, cost, eventName}));
         }}></AddItemComponent>
 
-      <FlatList
-        data={ev?.items}
-        renderItem={({item}) => (
-          <View style={styles.item}>
-            <Text>Name : {item.name}</Text>
-            <Text>Cost</Text>
-            <TextInput
-              style={{height: 40, backgroundColor: 'pink'}}
+      <List.Section>
+        {ev?.items.map((item) => (
+          <>
+          <List.Item
+            title={item.name}
+          />
+          <TextInput
               keyboardType={'number-pad'}
-              placeholder="Item name"
+              label="Cost"
               onChangeText={(text) => onUpdateCost(item, text)}
               defaultValue={item.cost.toString()}></TextInput>
-          </View>
-        )}></FlatList>
+          </>
+        ))}
+      </List.Section>
     </View>
   );
 };
@@ -127,41 +52,29 @@ const AddItemComponent = (props) => {
   const [name, setName] = useState('');
   const [cost, setCost] = useState(0);
   return (
-    <View style={styles.centeredView}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={isVisible}
-        onRequestClose={onCancel}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Add Event</Text>
-            <TextInput
-              style={{height: 40}}
-              placeholder="Item name"
-              onChangeText={(text) => setName(text)}></TextInput>
-            <TextInput
-              style={{height: 40}}
-              keyboardType="number-pad"
-              placeholder="Cost"
-              onChangeText={(text) => setCost(Number(text))}></TextInput>
+    <Portal>
+      <Modal visible={isVisible} onDismiss={onCancel}>
+        <TextInput
+          style={{height: 40}}
+          label="Item name"
+          onChangeText={(text) => setName(text)}></TextInput>
+        <TextInput
+          keyboardType="number-pad"
+          label="Cost"
+          onChangeText={(text) => setCost(Number(text))}></TextInput>
 
-            <TouchableOpacity
-              style={{...styles.openButton, backgroundColor: '#2196F3'}}
-              onPress={onCancel}>
-              <Text style={styles.textStyle}>Cancel</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.openButton}
-              onPress={() => {
-                onSubmit({name, cost});
-              }}>
-              <Text style={styles.textStyle}>Submit</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+        <Button mode="contained" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button
+          mode="contained"
+          onPress={() => {
+            onSubmit({name, cost});
+          }}>
+          Submit
+        </Button>
       </Modal>
-    </View>
+    </Portal>
   );
 };
 
